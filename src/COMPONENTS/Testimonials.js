54bb46d999afad1c3cf1c659e2671c6e69c50476
huzaifac137 <huzmanbutt@gmail.com>
2022-10-27 21:48:25 +0500

    ADDED SLIDER

--- src/COMPONENTS/Testimonials.js
+++ src/COMPONENTS/Testimonials.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import huzaifa from "../img/huzaifa.jpg";
 import faraz from "../img/faraz.jpg";
 import haroon from "../img/haroon.jpg";
 import tabarak from "../img/tabarak.jpg";
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
+import {AiOutlineArrowRight ,AiOutlineArrowLeft } from "react-icons/ai";
+
 
 function Testimonials(props) {
+
+    const[disablePrev , setDisablePrev] = useState(false);
+    const[disableNext , setDisableNext] = useState(false);
+
+
+    const swiperRef = useRef();
+
+    const handlePrev=()=>
+    {
+        swiperRef.current?.slidePrev();
+         setDisablePrev(swiperRef.current?.isBeginning) ;
+         setDisableNext(swiperRef.current?.isEnd);
+    }
+
+    const handleNext=()=>
+    {
+        swiperRef.current?.slideNext();
+        setDisablePrev(swiperRef.current?.isBeginning) ;
+        setDisableNext(swiperRef.current?.isEnd);
+    }
+
 
     const testimonials = [
         {
@@ -30,17 +56,36 @@ function Testimonials(props) {
 
     return (
         <div id='testimonial'>
-               <div className=" container flex flex-col  mx-auto p-4 space-y-10 md:space-y-0 
+               <div className=" container flex flex-col  mx-auto p-4 space-y-10
                mt-10 items-center w-full max-w-full">
 
-                      <h2 className='text-center text-brightRed text-4xl font-bold'>Testimonials </h2> 
+                      <h2 className='text-center text-brightRed text-4xl font-bold'>Testimonials </h2>
 
-                      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-auto gap-y-10 md:gap-x-10 lg:gap-y-0
-                      p-2  w-full max-w-full  place-items-center">
                                  
-                 { testimonials.map((item)=>  
+                  
+    <Swiper 
+      onBeforeInit={(swiper)=>{
+        swiperRef.current = swiper;
+      }}
+      
+      spaceBetween={50}
+      slidesPerView={1}
+      className="w-3/4 md:w-2/4 lg:w-2/5  p-5  space-y-5 cursor-pointer
+      border-l-darkGrayishBlue    border-b-darkGrayishBlue border-t-0 border-r-0 rounded border dropdown-shadow-lg shadow-md "
+    >
 
-                 <div key={item.name} className="flex flex-col items-center max-w-md p-4 mt-10 space-y-5
+    { testimonials.map((item)=>     <SwiperSlide key={item.name} >
+    <img className='w-44 h-44 mx-auto  md:w-28 md:h-28 rounded-full  ' src={item.img} alt="" />    
+     <h3 className='text-center text-brightRed'>{item.name}</h3> 
+     <p className='text-center text-darkGrayishBlue py-3'> It was a great experience working with tech legion</p>
+    </SwiperSlide> )}
+    </Swiper>
+    <div className='flex flex-row mx-auto space-x-10'>
+    <button  onClick={handlePrev }> { disablePrev===false ? <AiOutlineArrowLeft/> : null }   </button>
+
+    <button onClick={handleNext} > { disableNext===false ? <AiOutlineArrowRight/> : null} </button>
+    </div>
+             {/*   <div key={item.name} className="flex flex-col items-center max-w-md p-4 mt-10 space-y-5
                     border-l-0  border-b-darkGrayishBlue border-t-0 border-r-0 rounded border dropdown-shadow-lg shadow-md ">
                                      
                                         
@@ -49,11 +94,11 @@ function Testimonials(props) {
                                      
                                     <h3 className='text-center text-brightRed'>{item.name}</h3> 
                                     <p className='text-center text-darkGrayishBlue py-3'> It was a great experience working with tech legion</p>
-                                 </div>  )}
+                 </div>  )}   */}
 
                       </div>
                </div>
-        </div>
+       
     );
 }
 
